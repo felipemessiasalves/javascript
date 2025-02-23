8fd7489fdb5efac189b921a53ba77b6c7cfd3f49
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-02-23 01:31:39 -0300

    atualizei a aula 18

--- aula18/assets/js/main.js
+++ aula18/assets/js/main.js
@@ -17,7 +17,31 @@ form.addEventListener('submit', function(e) {
         setRes('Altura inválida', false)
         return
     }
+
+    const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc) 
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`
+
+    setRes(msg, true) 
 })
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if (imc >= 39.9) return nivel[5]
+    if (imc >= 34.9) return nivel[4]
+    if (imc >= 29.9) return nivel[3]
+    if (imc >= 24.9) return nivel[2]
+    if (imc >= 18.5) return nivel[1]
+    if (imc < 18.5) return nivel[0]
+}
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+}
 
 function criaP() {
     const p = document.createElement('p')
@@ -27,5 +51,15 @@ function criaP() {
 function setRes(msg, isValid) {
     const res = document.querySelector('#ires')
     res.innerHTML = ''
+
     const p = criaP() 
+    
+    if (isValid)  {
+        p.classList.add('p-res')
+    } else {
+        p.classList.add('bad') 
+    }
+
+    p.innerHTML = msg
+    res.appendChild(p)
 }
