7aea0d3e0df5038990d21f9fe6dfb41bc72f2931
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-03-05 19:30:32 -0300

    criei a aula 38

---
+++ aula38/aula38.js
@@ -0,0 +1,10 @@
+// Valor por referência
+//                    0         1        2         3          4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
+// const novo = nomes.slice(1, -2)
+// console.log(novo)
+
+// const nome = 'Felipe, Messias, Alves, Corrêa'
+const nomes = ['Felipe', 'Messias', 'Alves', 'Corrêa']
+const nome = nomes.join(' ')
+console.log(nome)
