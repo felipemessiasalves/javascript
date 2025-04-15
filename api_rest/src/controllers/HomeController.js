32d325c4fa834d17d88615be48d95845111f1461
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-04-14 22:55:27 -0300

    api_rest

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from "../models/Aluno.js";
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: "Felipe",
+      sobrenome: "Alves",
+      email: "g5a0U@example.com",
+      idade: 20,
+      peso: 68,
+      altura: 1.75,
     });
+    res.json(novoAluno);
   }
 }
 
