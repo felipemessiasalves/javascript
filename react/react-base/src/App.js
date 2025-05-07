8b48044fafb296687f9da4d02e0a88404fcfbf69
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-05-07 12:23:17 -0300

    adicionei o Redux

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -1,6 +1,8 @@
 import { Router } from "react-router-dom/cjs/react-router-dom.min";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import history from "./services/history";
 import GlobalStyle from "./styles/GlobalStyles";
 import Header from "./components/header";
@@ -8,12 +10,14 @@ import Routes from "./routes";
 
 function App() {
   return (
-    <Router history={history}>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} className="toast-container" />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </Router>
+    </Provider>
   );
 }
 
--- react/react-base/src/pages/login/index.js
+++ react/react-base/src/pages/login/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 import { Container } from "../../styles/GlobalStyles";
 import { Title, Paragraph } from "./styled";
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: "BOTAO_CLICADO",
+    });
+  }
+
   return (
     <Container>
       <Title isRed>
@@ -11,7 +22,9 @@ export default function Login() {
         <small>Oie</small>
       </Title>
       <Paragraph>Lorem ipsum dolor sit amet.</Paragraph>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
