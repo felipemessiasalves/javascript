24686f7f8604ddae70d6964b136a87f2758574e8
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-05-16 11:57:10 -0300

    criei a pasta do projeto real

---
+++ react/consumirapi/src/pages/Login/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+import { Container } from "../../styles/GlobalStyles";
+import { Title, Paragraph } from "./styled";
+import * as exampleActions from "../../store/modules/example/actions";
+
+export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch(exampleActions.clicaBotaoRequest());
+  }
+
+  return (
+    <Container>
+      <Title isRed>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragraph>Lorem ipsum dolor sit amet.</Paragraph>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
+    </Container>
+  );
+}
