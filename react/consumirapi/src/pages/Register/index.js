30cf6a5c41b49684a623658341606564e1c389ed
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-06-10 23:07:09 -0300

    atualizei a api

--- react/consumirapi/src/pages/Register/index.js
+++ react/consumirapi/src/pages/Register/index.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { isEmail } from "validator";
 
 import { Container } from "../../styles/GlobalStyles";
+import { Form } from "./styled";
 
 export default function Register() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error("Nome deve ter entre 3 e 255 caracteres.");
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error("E-mail inválido.");
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error("Senha deve ter entre 6 e 50 caracteres.");
+    }
+
+    if (formErrors) return;
+  }
+
   return (
     <Container>
-      <h1>Register</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu nome"
+          />
+        </label>
+
+        <label htmlFor="email">
+          E-mail:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Seu e-mail"
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Sua senha"
+          />
+        </label>
+
+        <button type="submit">Criar minha conta</button>
+      </Form>
     </Container>
   );
 }
--- react/consumirapi/src/pages/Register/styled.js
+++ react/consumirapi/src/pages/Register/styled.js
@@ -1,3 +1,27 @@
 import styled from "styled-components";
+import * as colors from "../../config/colors";
 
-export const Paragraph = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  gap: 20px;
+
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-top: 5px;
+
+    &:focus {
+      outline: none;
+      border-color: ${colors.primaryColor};
+    }
+  }
+`;
