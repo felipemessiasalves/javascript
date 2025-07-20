1a7b6a9fa0ab358faca44b6afe56fa66adcc8723
Felipe Corrêa <felipemessiasalvescorrea010@gmail.com>
2025-07-19 21:54:41 -0300

    update files

--- react/consumirapi/src/store/modules/auth/sagas.js
+++ react/consumirapi/src/store/modules/auth/sagas.js
@@ -1,10 +1,27 @@
 import { call, put, all, takeLatest } from "redux-saga/effects";
 import { toast } from "react-toastify";
+import { get } from "lodash";
 import * as actions from "./actions";
 import * as types from "../types";
+import history from "../../../services/history";
 
 function* loginRequest({ payload }) {
-  console.log("SAGA", payload);
+  try {
+    yield put(actions.loginSuccess());
+    toast.success("Você fez login");
+    history.push(payload.prevPath);
+  } catch {
+    toast.error("Deu erro.");
+    yield put(actions.loginFailure());
+  }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+function persistRehydrate(payload) {
+  const token = get(payload, "auth.token", "");
+  if (!token) return;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
